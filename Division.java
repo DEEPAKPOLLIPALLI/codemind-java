06a83453597763bd5a5d948b8af4a41bdc9b870b
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-11-27 12:08:23 +0530

    import java.util.Scanner; class Pretty {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=a/b;         System.out.println(c);         sc.close();
        }
       
    }

---
+++ Division.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Pretty
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a/b;
+        System.out.println(c);
+        sc.close();
+        
+    }
+   
+}
